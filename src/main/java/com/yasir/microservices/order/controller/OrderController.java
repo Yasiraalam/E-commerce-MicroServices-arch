2bc0812a2029713fa99cd113f7592f4599d36abc
Yasiralam <yasiralam981@gmail.com>
2025-04-18 16:24:51 +0530

    [✔]added OrderController, OrderRepository, OrderServiceApplication files and also flyway migration db

--- src/main/java/com/yasir/microservices/order/controller/OrderController.java
+++ src/main/java/com/yasir/microservices/order/controller/OrderController.java
@@ -4,16 +4,20 @@ package com.yasir.microservices.order.controller;
 import com.yasir.microservices.order.Dto.OrderRequest;
 import com.yasir.microservices.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
-@RequiredArgsConstructor
 public class OrderController {
+
 
     private final OrderService orderService;
 
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
--- src/test/java/com/yasir/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/yasir/microservices/order/OrderServiceApplicationTests.java
@@ -5,11 +5,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+
+		InventoryStubs.stubInventoryCall("iphone_15", 1);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
 }
