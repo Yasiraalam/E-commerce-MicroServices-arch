5ed2b8a5e76de434796412044bb8f421ec5a6cb9
Yasiralam <yasiralam981@gmail.com>
2025-04-21 00:13:04 +0530

    [✔] connected the inventory service with order service using open feign client

--- src/main/java/com/yasir/microservices/order/service/OrderService.java
+++ src/main/java/com/yasir/microservices/order/service/OrderService.java
@@ -2,6 +2,7 @@ package com.yasir.microservices.order.service;
 
 import com.yasir.microservices.order.Dto.OrderRequest;
 import com.yasir.microservices.order.Repository.OrderRepository;
+import com.yasir.microservices.order.client.InventoryClient;
 import com.yasir.microservices.order.model.Order;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -14,16 +15,23 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void PlaceOrder(OrderRequest orderRequest) {
-        //map orderRequest to order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        //save order to OrderRepository
-        orderRepository.save(order);
+        boolean ProductIsInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if(ProductIsInStock){
+            //map orderRequest to order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            //save order to OrderRepository
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product with skuCode "+ orderRequest.skuCode()+ " is not in Stock!");
+        }
+
 
     }
 }
